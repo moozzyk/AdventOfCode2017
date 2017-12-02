34cbb0a89f84d5943b58cdd8bf76ccfa85f1e2e2
moozzyk <moozzyk@gmail.com>
2017-12-02 10:58:03 -0800

    Day 2, Problem 2

--- Day02/solution.ml
+++ Day02/solution.ml
@@ -10,18 +10,38 @@ let read_lines name : string list =
 let split string separator =
   Str.split (Str.regexp separator) string
 
-let solve int_lists =
-    let rec min_max_diff l minimum maximum =
-        match l with
-        | [] -> maximum - minimum
-        | h::t -> min_max_diff t (min h minimum) (max h maximum) in
-  let diffs = List.map (fun item -> min_max_diff item (List.nth item 0) (List.nth item 0)) int_lists in
+(* Problem 1 *)
+let rec min_max_diff l minimum maximum =
+  match l with
+  | [] -> maximum - minimum
+  | h::t -> min_max_diff t (min h minimum) (max h maximum)
+
+(* Problem 2 *)
+let option_get x =
+  match x with
+  | Some c -> c
+  | None -> raise (Invalid_argument "Unexpected None value")
+
+let rec find_even_div list idx =
+  if idx < List.length list then
+    let dividend = List.nth list idx in
+    let divisor = List.find_opt (fun item -> (dividend != item) && (dividend mod item) = 0) list in
+    if divisor = None then
+      find_even_div list (idx + 1)
+    else
+      dividend / (option_get divisor)
+  else
+    raise (Invalid_argument "Unexpected condition")
+
+let solve int_lists f =
+  let diffs = List.map (fun item -> f item) int_lists in
   List.fold_left (+) 0 diffs
 
-let() =
-    let preprocess string =
-        let str_array = split string "\t" in
-        List.map (fun item -> int_of_string item) str_array in
-    let lines = read_lines "input.txt" in
-    let int_lists = List.map (fun item -> preprocess item) lines in
-    print_int (solve int_lists); print_endline ""
+let () =
+  let preprocess string =
+    let str_array = split string "\t" in
+    List.map (fun item -> int_of_string item) str_array in
+  let lines = read_lines "input.txt" in
+  let int_lists = List.map (fun item -> preprocess item) lines in
+  print_int (solve int_lists (fun item -> min_max_diff item max_int min_int)); print_endline "";
+  print_int (solve int_lists (fun item -> find_even_div item 0)); print_endline ""
