b12cb8c96b2758e7cffe63f72e56df188fce53be
moozzyk <moozzyk@gmail.com>
2017-12-03 21:43:33 -0800

    Day 4, Problem 2

--- Day04/solution.ml
+++ Day04/solution.ml
@@ -10,16 +10,31 @@ let read_lines name : string list =
   let split string separator =
     Str.split (Str.regexp separator) string
 
-let is_valid l =
-  let rec has_dups words dict =
-    match words with
-    | [] -> 1
-    | h::t -> if Hashtbl.mem dict h then 0 else begin Hashtbl.add dict h "";has_dups t dict end
-  in
-  let words = split l " " in
-  let dict = Hashtbl.create 100 in
-  has_dups words dict
+let explode s =
+  let rec exp i l =
+    if i < 0 then l else exp (i - 1) (s.[i] :: l) in
+  exp (String.length s - 1) []
+
+let rec has_dups words dict =
+  match words with
+  | [] -> 1
+  | h::t -> if Hashtbl.mem dict h then 0 else begin Hashtbl.add dict h "";has_dups t dict end
+
+let rec has_dup_anagrams words dict =
+  match words with
+  | [] -> 1
+  | h::t ->
+    let w = explode h in
+    let sorted_w = List.sort (fun l r -> if l < r then 1 else if l = r then 0 else - 1) w in
+    if Hashtbl.mem dict sorted_w then 0 else begin Hashtbl.add dict sorted_w "";has_dup_anagrams t dict end
+
 
 let () =
   let lines = read_lines "input.txt" in
-  print_int (List.fold_left (fun a i -> a + (is_valid i)) 0 lines)
+  let is_valid l f =
+    let words = split l " " in
+    let dict = Hashtbl.create 100 in
+    f words dict
+  in
+  print_int (List.fold_left (fun a i -> a + (is_valid i has_dups)) 0 lines); print_endline "";
+  print_int (List.fold_left (fun a i -> a + (is_valid i has_dup_anagrams)) 0 lines); print_endline "";
