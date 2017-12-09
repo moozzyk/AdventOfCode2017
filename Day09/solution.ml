13044118b4e58ffed4f2acc8362bc0473becd774
moozzyk <moozzyk@gmail.com>
2017-12-08 22:34:55 -0800

    Day 9, Problem 1

---
+++ Day09/solution.ml
@@ -0,0 +1,36 @@
+let read_lines name : string list =
+  let ic = open_in name in
+  let try_read () =
+    try Some (input_line ic) with End_of_file -> None in
+  let rec loop acc = match try_read () with
+    | Some s -> loop (s :: acc)
+    | None -> close_in ic; List.rev acc in
+  loop []
+
+let explode s =
+  let rec exp i l =
+    if i < 0 then l else exp (i - 1) (s.[i] :: l) in
+  exp (String.length s - 1) []
+
+let rec solve char tail garbage indent score =
+  if tail = [] then
+    (* tail is empty but need to add one since char should be the last '}' *)
+    1 + score
+  else if char = '!' then
+    solve (List.hd (List.tl tail)) (List.tl (List.tl tail)) garbage indent score
+  else if garbage then
+    if char = '>' then
+      solve (List.hd tail) (List.tl tail) false indent score
+    else
+      solve (List.hd tail) (List.tl tail) garbage indent score
+  else
+    match char with
+    | '<' -> solve (List.hd tail) (List.tl tail) true indent score
+    | '{' -> solve (List.hd tail) (List.tl tail) false (indent + 1) score
+    | '}' -> solve (List.hd tail) (List.tl tail) false (indent - 1) (score + indent)
+    | _ -> solve (List.hd tail) (List.tl tail) false indent score
+
+let () =
+  let line = List.hd (read_lines "input.txt") in
+  let chars = explode line in
+  print_int (solve (List.hd chars) (List.tl chars) false 0 0); print_endline ""
