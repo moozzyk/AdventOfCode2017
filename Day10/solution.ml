f0913a9110cbb31f627895c326f1b544ebe0444b
moozzyk <moozzyk@gmail.com>
2017-12-10 20:58:46 -0800

    Day 10, Problem 2

--- Day10/solution.ml
+++ Day10/solution.ml
@@ -10,6 +10,11 @@ let read_lines name : string list =
 let split string separator =
   Str.split (Str.regexp separator) string
 
+let explode s =
+  let rec exp i l =
+    if i < 0 then l else exp (i - 1) (s.[i] :: l) in
+  exp (String.length s - 1) []
+
 let rec print_list l =
   match l with
   | [] -> print_endline ""
@@ -29,7 +34,8 @@ let solve list lengths =
   let size = List.length list in
   let rec get_list list skip lengths hd_idx =
     match lengths with
-      | [] -> (list, (List.nth list hd_idx) * (List.nth list ((hd_idx + 1) mod size)))
+      | [] -> let (left, right) = rotate list hd_idx [] in
+        (right@left)
       | hd::tl ->
         let (left, right) = rotate list hd [] in
         let left = List.rev left in
@@ -37,18 +43,40 @@ let solve list lengths =
         let pivot = (skip + hd) mod size in
         let (left, right) = rotate list pivot [] in
         let hd_idx = if hd_idx <= pivot then hd_idx + (size - pivot) else hd_idx - pivot in
-        (* print_string "pivot: "; print_int pivot; print_endline "";
-        print_list (right@left);
-        print_string "hd_idx: "; print_int hd_idx; print_endline ""; print_endline "---";
-        print_string "nth "; print_int (List.nth (right@left) hd_idx); print_endline ""; *)
         get_list (right@left) (skip + 1) tl hd_idx
   in
-  let (out_list, result) = get_list list 0 lengths 0 in
-  result
+  get_list list 0 lengths 0
+
+let problem_1 line list =
+  let lengths = List.map (fun i -> int_of_string i) (split line ",") in
+  let result = solve list lengths in
+  print_int (List.hd result * (List.hd (List.tl result))); print_endline ""
+
+(* Problem 2 *)
+let problem_2 line list =
+  let chars = explode line in
+  let lengths = List.map (fun c -> int_of_char c) chars in
+  let lengths = (lengths@[17; 31; 73; 47; 23]) in
+  let lengths = (lengths@lengths) in
+  let lengths = (lengths@lengths) in
+  let lengths = (lengths@lengths) in
+  let lengths = (lengths@lengths) in
+  let lengths = (lengths@lengths) in
+  let lengths = (lengths@lengths) in
+  let out_list = solve list lengths in
+  let rec fold_left list (idx, acc, res) =
+    match list with
+    | [] -> res
+    | hd::tl ->
+      if ((idx + 1) mod 16) = 0 then
+        fold_left tl (idx + 1, 0, res@[(acc lxor hd)])
+      else
+        fold_left tl (idx + 1, acc lxor hd, res)
+  in
+  let compressed = fold_left out_list (0, 0, []) in
+  List.iter (fun i -> Printf.printf "%02x" i) compressed; print_endline ""
 
 let () =
   let line = List.hd (read_lines "input.txt")  in
-  let lengths = List.map (fun i -> int_of_string i) (split line ",") in
-  let list = range 255 0 [] in
-  let result = solve list lengths in
-  print_int result; print_endline "";
+  problem_1 line (range 255 0 []);
+  problem_2 line (range 255 0 [])
