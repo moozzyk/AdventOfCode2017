caa7c78205ac3545055da441c956459b3748dcd5
moozzyk <moozzyk@gmail.com>
2017-12-11 22:49:51 -0800

    Day 12, Problem 2

--- Day12/solution.ml
+++ Day12/solution.ml
@@ -37,10 +37,31 @@ let solve_1 graph =
   in
   print_int (Hashtbl.fold (fun n v acc -> acc + if dfs graph n (Hashtbl.create 2000) then 1 else 0) graph 0); print_endline ""
 
+let solve_2 graph =
+  let rec dfs graph node visited =
+    if Hashtbl.mem visited node then
+      false
+    else
+    begin
+      Hashtbl.add visited node "";
+      let child_nodes = Hashtbl.find graph node in
+      let res = List.map (fun n -> dfs graph n visited) child_nodes in
+      List.fold_left (fun i acc -> i || acc) false res
+    end
+  in
+  let fold_nodes graph node visited =
+    let visited_before = Hashtbl.length visited in
+    let _ = dfs graph node visited in
+    if visited_before < (Hashtbl.length visited) then 1 else 0
+  in
+  let visited = Hashtbl.create 2000 in
+  print_int (Hashtbl.fold (fun n v acc -> acc + fold_nodes graph n visited) graph 0); print_endline ""
+
 let () =
   let lines = read_lines "input.txt" in
   let graph = build_graph lines in
-  solve_1 graph
+  solve_1 graph;
+  solve_2 graph;
 
 
 
