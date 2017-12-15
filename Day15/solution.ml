cebf0640e0930d75f9436b0d095905c586c2b7e8
moozzyk <moozzyk@gmail.com>
2017-12-14 21:50:44 -0800

    Day 15, Problem 2

--- Day15/solution.ml
+++ Day15/solution.ml
@@ -1,13 +1,17 @@
-let rec next_values inputA factorA inputB factorB =
-  let next_value input factor = (input * factor) mod 2147483647 in
-  ((next_value inputA factorA), (next_value inputB factorB))
+let rec next_value input factor mod_val =
+  let res = (input * factor) mod 2147483647 in
+  if (res mod mod_val) = 0 then res else next_value res factor mod_val
 
-let rec find_matching_pairs i inputA inputB res =
-  if (i = 40000000) then res
+let rec find_matching_pairs i (inputA, factorA, modA) (inputB, factorB, modB) res =
+  if (i = 0) then res
   else
-    let (inputA, inputB) = next_values inputA 16807 inputB 48271 in
-    find_matching_pairs (i + 1) inputA inputB (res + if (inputA land 65535) = (inputB land 65535) then 1 else 0)
+    let inputA = next_value inputA factorA modA in
+    let inputB = next_value inputB factorB modB in
+    let res = res + if (inputA land 65535) = (inputB land 65535) then 1 else 0 in
+    find_matching_pairs (i - 1) (inputA, factorA, modA) (inputB, factorB, modB) res
 
 let () =
-  let res = find_matching_pairs 0 703 516 0 in
-  print_int res
+  let inputA = 703 in
+  let inputB = 516 in
+  print_int (find_matching_pairs 40000000 (inputA, 16807, 1) (inputB, 48271, 1) 0); print_endline "";
+  print_int (find_matching_pairs 5000000 (inputB, 16807, 4) (inputB, 48271, 8) 0); print_endline "";
